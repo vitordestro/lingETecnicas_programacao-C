2ea7f68c67b75a551ec656f82642ce1547b57e46
Vitor Destro <vitordestro08@gmail.com>
2026-08-13 16:33:58 -0300

    conversão de um número n em decimal para binaria em representação de bits

---
+++ listaDeExercicios/lista1/ex3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+ int n;
+    int resultado1, resultado2, resultado3, resultado4, resultado5, resultado6;
+
+    printf("Insira um numero entre 0 a 64: ");
+    scanf("%d", &n);
+
+    resultado1 = (n / 32) % 2;
+    resultado2 = (n / 16) % 2;
+    resultado3 = (n / 8) % 2;
+    resultado4 = (n / 4) % 2;
+    resultado5 = (n / 2) % 2;
+    resultado6 = n % 2;
+
+    printf("%d%d%d%d%d%d",resultado1, resultado2,  resultado3,  resultado4, resultado5,  resultado6);
+  
+return 0;
+}
